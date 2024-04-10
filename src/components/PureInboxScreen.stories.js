68c911ffbf32ff16f4f06bcd1e50510d363f8c66
Jelena Cojic <jelena.cojic@swissmarketplace.group>
2024-04-10 22:59:54 +0200

    Add PureInboxScreen stories

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,14 @@
+import PureInboxScreen from './PureInboxScreen.vue';
+
+export default {
+  component: PureInboxScreen,
+  title: 'PureInboxScreen',
+  tags: ['autodocs'],
+};
+
+export const Default = {};
+
+export const Error = {
+  args: { error: true },
+};
+
